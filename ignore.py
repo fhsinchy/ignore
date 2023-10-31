1e27b469db528fab22fb8473dc4fb78934418b86
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-10-31 13:24:50 +0600

    Added possibility of appending to an existing file

--- ignore.py
+++ ignore.py
@@ -1,6 +1,7 @@
-from os import remove
+# from os import remove
 from sys import argv, exit
 from requests import Session, ConnectionError
+
 
 def main():
     file_name = '.gitignore'
@@ -16,28 +17,29 @@ def main():
 
                 for template in response.json():
                     print(template)
-            
+
             else:
                 argument = argument.capitalize()
 
                 response = session.get(f"https://api.github.com/gitignore/templates/{argument}",
-                                headers={ 'Accept': 'application/vnd.github+json' })
-                
+                                       headers={'Accept': 'application/vnd.github+json'})
+
                 if response.status_code == 404:
                     exit(f"NO TEMPLATE FOUND FOR: {argument}")
 
-                try:
-                    remove(file_name)
-                except OSError:
-                    pass
+                # try:
+                #     remove(file_name)
+                # except OSError:
+                #     pass
 
                 with open(file_name, 'a', encoding='utf-8') as toc_file:
                     toc_file.write(response.json()['source'])
-            
+
         except ConnectionError:
             exit("OOPS! COULDN'T CONNECT TO GITHUB.")
     else:
         exit('NO ARGUMENT PASSED.')
+
 
 if __name__ == '__main__':
     main()
